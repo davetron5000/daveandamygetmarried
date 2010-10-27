97414075d0e5cf917a60e8bb7107dd2abc338a23
Dave Copeland <davetron5000@gmail.com>
2010-10-26 20:46:42 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DaveandamygetmarriedCom::Application.config.secret_token = '5934f0527730ac3da016ddd16e037418fb177ec3557c9409e1b6638df085b5b9c0a882be6d7864f230382118f01639a1f1c9c97bce00a672db8b8edf2ac63a6f'
