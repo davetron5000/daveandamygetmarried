758a2949aa6f96f9ebafc2455a5674fa3ef9b112
Dave Copeland <davetron5000@gmail.com>
2010-12-18 17:26:59 -0500

    more changes and whatnot

--- app/controllers/us_controller.rb
+++ app/controllers/us_controller.rb
@@ -1,8 +1,11 @@
 class UsController < ApplicationController
+  KNOWN_MEETERS = ['Amy','Dave','Rudy']
   def who
   end
 
   def met
+    @according_to = params[:who]
+    @known = KNOWN_MEETERS.include? params[:who]
   end
 
   def engagement
