e253bb9cea8ea758ff5bc868ac4b1b28c2b6c49e
Dave Copeland <davec@naildrivin5.com>
2014-12-20 11:16:53 -0500

    attempt to migrate Heroku stacks

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module DaveandamygetmarriedCom
   class Application < Rails::Application
+    ActionController::Base.config.relative_url_root = ''
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
