08cd94e52edc60479389346f6c62d667c9a1d352
Dave Copeland <david.copeland@livingsocial.com>
2019-05-28 10:35:48 -0400

    Revert "upgrade to Rails 5"
    This reverts commit aafe0a57e047b97395539897152ae345e5302e4a.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'http://rubygems.org'
 
-gem 'rails', '~> 5.0'
-ruby "2.6.3"
+gem 'rails', '~> 3.0'
+ruby "2.3.0"
 
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'thin'
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,6 +20,10 @@ DaveandamygetmarriedCom::Application.configure do
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
 
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
@@ -28,5 +32,4 @@ DaveandamygetmarriedCom::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-  config.eager_load  = false
 end
